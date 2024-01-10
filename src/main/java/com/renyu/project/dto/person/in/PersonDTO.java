0fa6a1f42dbd262eaa0685fabc3d0a8cc2c86217
Yuri Rennan <yuri.rennan@sogo.com.br>
2024-01-10 16:13:13 -0300

    Adição de nova validação e annotation de serialização

--- src/main/java/com/renyu/project/dto/person/in/PersonDTO.java
+++ src/main/java/com/renyu/project/dto/person/in/PersonDTO.java
@@ -1,17 +1,22 @@
 package com.renyu.project.dto.person.in;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.renyu.project.model.Person;
 import lombok.Data;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 @Data
 public class PersonDTO {
 
     @NotBlank
+    @JsonProperty("first_name")
     private String firstName;
     @NotBlank
+    @JsonProperty("last_name")
     private String lastName;
+    @Email
     @NotBlank
     private String email;
 
